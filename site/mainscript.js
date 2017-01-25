ac6ad4a7d4e139273504a6e07eaa84af20555079
Sanjanu <anukushii@gmail.com>
2017-01-25 13:44:05 -0600

    new changes

--- site/mainscript.js
+++ site/mainscript.js
@@ -1,1 +1,20 @@
-mainscript.js
+
+  function displayImage()
+   {
+
+        console.log(this);
+  	 	var obj=document.getElementById("Picture1");
+        obj.className=('show');
+        var obj1=document.getElementById("Picture2");
+        obj1.className=('show');
+        var obj2=document.getElementById("Picture3");
+        obj2.className=('show');
+
+
+
+       // document.querySelector("button")
+       //.addEventListener("click", displayImage);
+   }
+  	 
+ document.querySelector("button")
+ onclick = displayImage;
