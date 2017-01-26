2de751364a6dd6499ad97158b8fc394cc77d6169
Sanjanu <anukushii@gmail.com>
2017-01-26 14:18:29 -0600

    ootstarp

--- site/mainscript.js
+++ site/mainscript.js
@@ -1,20 +1,30 @@
+var names = new Array("hiphop", "classical", "bollywood");
 
-  function displayImage()
-   {
+       function displayImage(event)
+       {
+       	console.log(event);
 
         console.log(this);
   	 	var obj=document.getElementById("Picture1");
-        obj.className=('show');
+        obj.className='show';
+        obj.addEventListener("mousemove", func);
+        
         var obj1=document.getElementById("Picture2");
-        obj1.className=('show');
+        obj1.className='show';
         var obj2=document.getElementById("Picture3");
-        obj2.className=('show');
+        obj2.className='show';
+
+        }
+    
+function func()
+{  
+ document.getElementById("textHover").innerHTML = names[0]; 
+  
+}
 
 
 
-       // document.querySelector("button")
-       //.addEventListener("click", displayImage);
-   }
-  	 
  document.querySelector("button")
- onclick = displayImage;
+     onclick=  displayImage;
+  	 
+ 
