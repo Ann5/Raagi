309d7967c21837cd5d233a78acee128fd38431ae
Sanjanu <anukushii@gmail.com>
2017-02-01 14:51:58 -0600

    teachers

--- site/mainscript.js
+++ site/mainscript.js
@@ -1,4 +1,4 @@
-var names = new Array("hiphop", "classical", "bollywood");
+var names = new Array("For example, it’s possible to look for elements with a class name of .box which are only inside a element with first-line as the id. Any items with the box class that aren’t inside of the first-line element, won’t be returned.", "classical", "bollywood");
 
        function displayImage(event)
        {
@@ -7,12 +7,35 @@ var names = new Array("hiphop", "classical", "bollywood");
         console.log(this);
   	 	var obj=document.getElementById("Picture1");
         obj.className='show';
-        obj.addEventListener("mousemove", func);
+       // obj.addEventListener("mouseenter", func);
+        
+         //	$( 'img').mouseenter(function(event) {
+        //  $( "p", this ).text( names[0] );
+        // });
+
+        $(document).ready(function () {
+         $('img').hover(function () {
+        $(this).stop().animate({
+            opacity: .4
+        }, 200);
+        $('.textHover').removeClass('hide');
+        $('#textHover').text(names[0]);
+
+         }, function () {
+        $(this).stop().animate({
+            opacity: 1
+        }, 500);
+        $('#textHover').addClass('hide');
+        
+        });
+        });
         
         var obj1=document.getElementById("Picture2");
         obj1.className='show';
+  
         var obj2=document.getElementById("Picture3");
         obj2.className='show';
+        
 
         }
     
@@ -22,9 +45,9 @@ function func()
   
 }
 
+  
 
 
- document.querySelector("button")
-     onclick=  displayImage;
+ document.querySelector("button").onclick= displayImage;
   	 
  
